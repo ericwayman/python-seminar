dbe528df0d65a77d5fd0352a9937d908e45e8f43
Eric Wayman <ewayman@berkeley.edu>
2013-09-16 16:16:28 -0700

    First draft.  Basic functionality of the calculate module.

--- hw2/CalCalc.py
+++ hw2/CalCalc.py
@@ -1,8 +1,16 @@
-import urllib2
+import re, urllib, urllib2
 
 def calculate(input_string):
 	try:
 		return eval(input_string, {'__builtins__':{}})
-	except NameError:
-		
+	except (NameError, SyntaxError):
+		f={'appid':'UAGAWR-3X6Y8W777Q','input': input_string}
+		api_url = 'http://api.wolframalpha.com/v2/query?' + urllib.urlencode(f)
+		xml_response = urllib2.urlopen(api_url).read()
+		regex = re.compile('<plaintext>(.*?)</plaintext>')
+		info = regex.findall(xml_response)
+		return info[1]
 
+if __name__== "__main__":
+	import sys
+	print calculate(str(sys.argv[1]))
