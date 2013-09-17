1211e15be79ea469d2497f9a7cb63c17d2b3b95f
Eric Wayman <ewayman@berkeley.edu>
2013-09-17 10:16:44 -0700

    Implemented argparse.

--- hw2/CalCalc.py
+++ hw2/CalCalc.py
@@ -4,7 +4,8 @@ def calculate(input_string):
 	try:
 		return eval(input_string, {'__builtins__':{}})
 	except (NameError, SyntaxError):
-		f={'appid':'UAGAWR-3X6Y8W777Q','input': input_string}
+		app_id = 'UAGAWR-3X6Y8W777Q'
+		f={'appid':app_id,'input': input_string}
 		api_url = 'http://api.wolframalpha.com/v2/query?' + urllib.urlencode(f)
 		xml_response = urllib2.urlopen(api_url).read()
 		regex = re.compile('<plaintext>(.*?)</plaintext>')
@@ -12,5 +13,12 @@ def calculate(input_string):
 		return info[1]
 
 if __name__== "__main__":
-	import sys
-	print calculate(str(sys.argv[1]))
+	#import sys
+	#print calculate(str(sys.argv[1]))
+	import argparse
+	parser=argparse.ArgumentParser(description="Query the Wolfrm|Alpha API")
+	parser.add_argument("query",
+						nargs=1,
+						help="Query for the Wolfram|Alpha API.  Don't forget to enclose query in quotes",
+						metavar="QUERY")
+	print calculate(" ".join(parser.parse_args().query))
