ce9ec4bafee9697d920f75a57067550dd2c164a4
Eric Wayman <ewayman@berkeley.edu>
2013-09-17 21:20:10 -0700

    Implemented a a command line flag -f that can be used to try to parse a float from the answer.  Also added a setup.py file to distribute the CalCalc module.

--- hw2/CalCalc.py
+++ hw2/CalCalc.py
@@ -1,6 +1,6 @@
 import re, urllib, urllib2
 
-def calculate(input_string):
+def calculate(input_string,return_float=False):
 	try:
 		return eval(input_string, {'__builtins__':{}})
 	except (NameError, SyntaxError):
@@ -9,8 +9,20 @@ def calculate(input_string):
 		api_url = 'http://api.wolframalpha.com/v2/query?' + urllib.urlencode(f)
 		xml_response = urllib2.urlopen(api_url).read()
 		regex = re.compile('<plaintext>(.*?)</plaintext>')
-		info = regex.findall(xml_response)
-		return info[1]
+		answer = regex.findall(xml_response)
+		
+		#parse a float from the Wolfram answer if requested
+		if return_float==True:
+			l = []
+			for x in answer[1].split():
+				try:
+					l.append(float(x))
+				except ValueError:
+					pass	
+			if len(l)==0:
+				print "There is no float to return or could not parse the correct float."
+			return l	
+		return answer[1]
 
 if __name__== "__main__":
 	#import sys
@@ -21,4 +33,15 @@ if __name__== "__main__":
 						nargs=1,
 						help="Query for the Wolfram|Alpha API.  Don't forget to enclose query in quotes",
 						metavar="QUERY")
-	print calculate(" ".join(parser.parse_args().query))
+	parser.add_argument("-f", "--is_float",
+						dest = 'is_float',
+						action ='store_const',
+						const = True,
+						default = False,
+						help ='The flag -f sets return_float = True',
+						)
+	print calculate(" ".join(parser.parse_args().query),parser.parse_args().is_float)
+
+
+#def test_1():
+#	assert abs(9. - calculate('3*3'))<.001
---
+++ hw2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='CalCalc',
+	version='1.0',
+	description='CalCalc module',
+	author='Eric Wayman',
+	author_email='ewayman@gmail.com',
+	py_modules=['CalCalc'],
+	)
