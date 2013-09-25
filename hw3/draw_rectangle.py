2e28d18a3a8a77c921f774f432518d1ee0b72558
Eric Wayman <ewayman@berkeley.edu>
2013-09-25 16:13:56 -0700

    Added functionality to draw_rectangle.py  Recorded the row indices of data points contained in a rectangle

--- hw3/draw_rectangle.py
+++ hw3/draw_rectangle.py
@@ -6,13 +6,16 @@ import matplotlib.pyplot as plt
 import matplotlib.patches
 
 class draw_rectangle:
-    def __init__(self, ax_array):
+    def __init__(self, ax_array,scatter_plots):
         self.click_loc = None
         self.ax_array = ax_array
         #initialize with an empty rectangle
         self.rect = matplotlib.patches.Rectangle((0,0),0,0,facecolor='orange') 
         #initialize the rectangle to be in the first axis.
         self.ax_array[0].add_patch(self.rect) 
+        self.scatter_plots = scatter_plots
+        self.data_pts = []
+    #the function to connect all the events
     def connect(self):
         self.cidpress = self.rect.figure.canvas.mpl_connect(
             'button_press_event', self.on_press)
@@ -30,13 +33,23 @@ class draw_rectangle:
             event.inaxes.add_patch(self.rect)
             self.click_loc = (event.xdata,event.ydata)
             print "rectangle created at (%f,%f)" %(self.click_loc[0],self.click_loc[1])
+            print event.inaxes
+            print type(event.inaxes)
+            print event.inaxes.get_geometry()
+            num=event.inaxes.get_geometry()[2]
+            #y_indx = num/4
+            #x_indx = num - 4*(num/4)-1
+            self.data_pts = self.scatter_plots[num].get_offsets()
         #otherwise don't do anything
         else: return
+        #draw the rectangle
         self.rect.figure.canvas.draw()
 
     def on_motion(self, event):
         'on motion we will create a rectangle if a click is registered'
+        #stops drawing the rectangle if the mouse hovers over points outside the current axis
         if event.inaxes != self.rect.axes: return
+        #Don't do anything if not clicked on a point
         if (self.click_loc is None): return
         x_0 = self.click_loc[0]
         y_0 = self.click_loc[1]
@@ -56,6 +69,11 @@ class draw_rectangle:
         else:
         	self.rect.set_y(event.ydata)
         	self.rect.set_height(y_0-event.ydata)
+        
+        #find data points contained in the rectangle
+        trutharray = self.rect.get_path().contains_points(self.data_pts)
+
+        #draw the rectangle
         self.rect.figure.canvas.draw()
     
     def on_release(self, event):
@@ -63,19 +81,21 @@ class draw_rectangle:
         self.click_loc = None
         self.rect.figure.canvas.draw()
     
-#make the grid of plots
+#make the grid of plots of the flower petal data
 path = './hw_3_data'
 fname = os.path.join(path, 'flowers.txt')
 tab = np.loadtxt(fname,delimiter=',',skiprows=1,usecols=(0,1,2,3))
-fig = plt.figure()
 ncols=tab.shape[1]
+gs = matplotlib.gridspec.GridSpec(ncols,ncols)
 ax_array=[]
+scat_array =[]
 for i in range(0,ncols):
     for j in range(0,ncols):
-        axis = fig.add_subplot(ncols,ncols,ncols*i+j+1)
-        axis.scatter(tab[:,i],tab[:,j])
-        ax_array.append(axis)    
-dr = draw_rectangle(ax_array)
+        axis = plt.subplot(gs[i,j])
+        scat=axis.scatter(tab[:,i],tab[:,j])
+        ax_array.append(axis)
+        scat_array.append(scat)    
+dr = draw_rectangle(ax_array,scat_array)
 dr.connect()
 plt.show()
 
--- hw3/readme.txt
+++ hw3/readme.txt
@@ -1,4 +1,4 @@
-draw_rectangle.py defines a draw_rectangle class.  The class displays a 4x4 grid of plots from the data in flowers.txt found in hw_3_data.  The class allows you to click on any subplot and trace a rectangle object in that class.  I wasn't able to finish the brushing functionality in time.
+draw_rectangle.py defines a draw_rectangle class.  The class displays a 4x4 grid of plots from the data in flowers.txt found in hw_3_data.  The class allows you to click on any subplot and trace a rectangle object in that class.  The class also tracks the row indices of the data points that a highlighted by a rectangle in any of the scatter plots.  I wasn't able to finish the brushing functionality.
 
 hw_3_1.py is the list of commands to reproduce the plot in HW31_example.  the plot is saved in hw_3_1_plot.png
 
